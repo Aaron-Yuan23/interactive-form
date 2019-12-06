17dc5f15faaf6267d0f24de946c99e9e9aff81f4
Aaron Yuan <aaronyuan27@gmail.com>
2019-12-06 13:45:48 -0700

    updated activities and T-shirt info section

--- js/script.js
+++ js/script.js
@@ -9,26 +9,95 @@ $(document).ready( ()=>{
     $("#colors-js-puns").append("<label id='color-theme'>Please select a T-shirt theme</label>");
 });
 
+/*-------- T-shirt Info section --------*/
 
+//display the color sector when the design is selected
 $("#design").on("change", ()=>{
     $('#color-theme').remove();
     $("#color").show();
-    if($("#design").val() === "default"){
+    if($("#design option:selected").val() === "default"){
         $("#color").hide();
         $("#colors-js-puns").append("<label id='color-theme'>Please select a T-shirt theme</label>"); 
     }
 });
 
+//display correspoding options when JS Puns is chosen
+$("#design").on("change", ()=>{
+  if($("#design").val() ==="js puns"){
+    $("#color option").remove();
+    $('#color').append('<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option><option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option><option value="gold">Gold (JS Puns shirt only)</option>');
+  }
+});
 
-
+$("#design").on("change", ()=>{
+  if($("#design").val() ==="heart js"){
+    $("#color option").remove();
+    $('#color').append('<option value="tomato">Tomato (I &#9829; JS shirt only)</option><option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option><option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> ');
+  }
+});
+     
 
 //dynamically changing the visability of other job field
 $("#title").on("change", ()=> {
-    if($("#title").val() == "other"){
+    if($("#title option:selected").val() === "other"){
       $("#other-role").show();
     }
     else{
       $("#other-role").hide(); 
     }
 });
+
+/*-----------register for activities----------*/
+let total = 0;
+const $totalHtml = $("<div class='total'><p></p></div>");
+$(".activities").append($totalHtml);
+
+//creating activities
+addActivity('all', 200);
+addActivity('js-frameworks', 100, 'express');
+addActivity('js-libs', 100, 'node');
+addActivity('express', 100, 'js-frameworks');
+addActivity('node', 100, 'js-libs');
+addActivity('build-tools', 100);
+addActivity('npm', 100);
+
+function addActivity(activityName, price, conflictName){
+  const $activity = $(".activities input[name="+ activityName +"]");
+
+  $activity.on("change", ()=>{
+    if($activity.is(":checked")) {
+			total += price;
+			$('.total p').text('Total: $' + total);
+			if(conflictName !== '') {
+				toggleCheckbox(conflictName, false);
+			}
+		}
+		else {
+			total -= price;
+			$('.total p').text('Total: $' + total);
+			if(conflictName !== '') {
+				toggleCheckbox(conflictName, true);
+			}
+		}
+  });
+}
+
+//disable the checkbox if there is a conflit
+function toggleCheckbox(activityName, enable){
+  const $activity = $(".activities input[name="+ activityName +"]");
+  //when the checkbox is unchecked
+  if(enable){
+    $activity.prop("disabled", false);
+    $activity.parent().removeClass("disabled");
+    $activity.parent().css('color', 'black');
+  }
+  //when the checkbox is checked
+  else{
+    $activity.prop("disabled", true);
+    $activity.parent().addClass("disabled");
+    $activity.parent().css('color', '#909497');
+  }
+}
+
+/*--------------payment Info---------------*/
 
