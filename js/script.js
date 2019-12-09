9da33ae988050c59498719441547b030ddf422b9
Aaron Yuan <aaronyuan27@gmail.com>
2019-12-08 22:04:02 -0700

    updated payment validation

--- js/script.js
+++ js/script.js
@@ -157,7 +157,7 @@ const $nameError = $("<div id='nameMsg2'><p>Please enter your name without speci
 $nameError.css("color", "red");
 $nameError.children().css("font-size","15px");
 
-//name field does not allow special characters and mutpe spaces
+//name field does not allow special characters and mutiple spaces
 let noSpecialChar = /^([a-zA-Z]+\s)*[a-zA-Z]+$/;
 
 
@@ -174,6 +174,63 @@ const $emailError = $("<div id='emailMsg2'><p>Please enter a valid email address
 $emailError.css("color", "red");
 $emailError.children().css("font-size","15px");
 
+//error message of at least one of the checkboxes needed to be selected
+const $checkboxMsg = $("<div id='checkboxMsg'><p>Please select at least one activity you wish to participate in</p></div>");
+$checkboxMsg.css("color", "red");
+$checkboxMsg.children().css("font-size","15px");
+
+//------------select payment method validation message-------------//
+//error messages of not selecting payment method//
+const $paymentMsg = $("<div id='paymentMsg'><p>Please select how you would like to pay</p></div>");
+$paymentMsg.css("color", "red");
+$paymentMsg.children().css("font-size","15px");
+
+//-------------credit card number validation messages-------------//
+const $cardNum = $("<div id='cardNum'><p>Please enter your credit card number</p></div>");
+$cardNum.css("color", "red");
+$cardNum.children().css("font-size","15px");
+
+const $cardError = $("<div id='cardError'><p>Please enter a credit card number between 13 and 16 digits</p></div>");
+$cardError.css("color", "red");
+$cardError.children().css("font-size","15px");
+
+//only numbers are allowed for credit card input
+const validCc = /^[0-9]*$/;
+
+const $validCreditNum = $("<div id='ccNumError'><p>Please enter numbers only</p></div>");
+$validCreditNum.css("color", "red");
+$validCreditNum.children().css("font-size","15px");
+
+//-------------zip code validation messages----------------//
+const $zipMsg = $("<div id='zipMsg'><p>Please enter your zip code</p></div>");
+$zipMsg.css("color", "red");
+$zipMsg.children().css("font-size","15px");
+
+const $zipError = $("<div id='zipError'><p>Please enter a 5 digit code</p></div>");
+$zipError.css("color", "red");
+$zipError.children().css("font-size","15px");
+
+//only numbers and characters are allowed for credit card input
+const validZip = /^[a-zA-Z0-9]+$/;
+
+const $validZip = $("<div id='validZipError'><p>Please enter numbers and letters only</p></div>");
+$validZip.css("color", "red");
+$validZip.children().css("font-size","15px");
+
+//----------------cvv validation messages------------------//
+const $cvvMsg = $("<div id='cvvMsg'><p>Please enter your cvv number</p></div>");
+$cvvMsg.css("color", "red");
+$cvvMsg.children().css("font-size","15px");
+
+const $cvvError = $("<div id='cvvError'><p>Please enter a 3 digit number</p></div>");
+$cvvError.css("color", "red");
+$cvvError.children().css("font-size","15px");
+
+const validCvv = /^[0-9]*$/;
+
+const $validCvv = $("<div id='validCvvError'><p>Please enter numbers only</p></div>");
+$validCvv.css("color", "red");
+$validCvv.children().css("font-size","15px");
 
 /*-------------form validation-----------*/
 
@@ -224,10 +281,135 @@ const validateEmail = (e)=>{
   }
 };
 
+//-----------checkbox validation--------------//
+const validateCheckbox = (e) =>{
+  if($(".activities input[type='checkbox']:checked").length ===0){
+    e.preventDefault();
+    $(".activities legend").after($checkboxMsg);
+  }
+  else{
+    $("#checkboxMsg").remove();
+  }
+};
+
+//-----------payment selector validation-----------//
+const validatePaymentSelector = (e) =>{
+  if($("#payment option:selected").val()==="select method"){
+    e.preventDefault();
+    $("#payment").before($paymentMsg);
+  }
+  else{
+    $("#paymentMsg").remove();
+  }
+};
+
+//----------------credit card number validation------------------//
+const validateCreditCard =(e)=>{
+  if($("#payment option:selected").val()==="credit card"){
+    if($("#cc-num").val() ===""){
+      e.preventDefault();
+      $("#cardError").remove();
+      $("#ccNumError").remove();
+      $("#cc-num").css("border", "2px solid red");
+      $("#cc-num").after($cardNum);
+    }
+    else if(!validCc.test($("#cc-num").val())){
+      e.preventDefault();
+      $("#cardNum").remove();
+      $("#cardError").remove();
+      $("#cc-num").css("border", "2px solid red");
+      $("#cc-num").after($validCreditNum);
+    }
+    else if($("#cc-num").val().length>16 ||$("#cc-num").val().length < 13){
+      e.preventDefault();
+      $("#cardNum").remove();
+      $("#ccNumError").remove();
+      $("#cc-num").css("border", "2px solid red");
+      $("#cc-num").after($cardError);
+    }
+    else{
+      $("#cardNum").remove();
+      $("#cardError").remove();
+      $("#ccNumError").remove();
+      $("#cc-num").css("border", "2px solid rgb(111, 157, 220)");
+    }
+  }
+};
+
+//------------------zip code validation---------------------//
+const validateZipCode =(e)=>{
+  if($("#payment option:selected").val()==="credit card"){
+    if($("#zip").val()===""){
+      e.preventDefault();
+      $("#zipError").remove();
+      $("#validZipError").remove();
+      $("#zip").css("border", "2px solid red");
+      $("#zip").after($zipMsg);
+    }
+    else if(!validZip.test($("#zip").val())){
+      e.preventDefault();
+      $("#zipMsg").remove();
+      $("#zipError").remove();
+      $("#zip").css("border", "2px solid red");
+      $("#zip").after($validZip);
+    }
+    else if($("#zip").val().length !=5){
+      e.preventDefault();
+      $("#zipMsg").remove();
+      $("#validZipError").remove();
+      $("#zip").css("border", "2px solid red");
+      $("#zip").after($zipError);
+    }
+    else{
+      $("#zipMsg").remove();
+      $("#zipError").remove();
+      $("#validZipError").remove();
+      $("#zip").css("border", "2px solid rgb(111, 157, 220)");
+    }
+  }
+};
+
+//-----------------cvv number validation--------------------//
+const validateCvv =(e)=>{
+  if($("#payment option:selected").val()==="credit card"){
+    if($("#cvv").val()===""){
+      e.preventDefault();
+      $("#cvvError").remove();
+      $("#validCvvError").remove();
+      $("#cvv").css("border", "2px solid red");
+      $("#cvv").after($cvvMsg);
+    }
+    else if(!validCvv.test($("#cvv").val())){
+      e.preventDefault();
+      $("#cvvMsg").remove();
+      $("#cvvError").remove();
+      $("#cvv").css("border", "2px solid red");
+      $("#cvv").after($validCvv);
+    }
+    else if($("#cvv").val().length !=3){
+      e.preventDefault();
+      $("#cvvMsg").remove();
+      $("#validCvvError").remove();
+      $("#cvv").css("border", "2px solid red");
+      $("#cvv").after($cvvError);
+    }
+    else{
+      $("#cvvMsg").remove();
+      $("#cvvError").remove();
+      $("#validCvvError").remove();
+      $("#cvv").css("border", "2px solid rgb(111, 157, 220)");
+    }
+  }
+};
 
 $(document).ready(()=>{
   $("#submit").on("click", (e)=>{
    validateName(e);
    validateEmail(e);
+   validateCheckbox(e);
+   validatePaymentSelector(e);
+   validateCreditCard(e);
+   validateZipCode(e);
+   validateCvv(e);
  });
 });
